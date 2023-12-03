f32212f866f1dd85c00615e27b850d23efcb6f3c
jiashu2000 <jiashuchen0414@gmail.com>
2023-12-02 20:39:47 -0800

    12/02

---
+++ Dec2/UncrossedLines.py
@@ -0,0 +1,17 @@
+# 1035. Uncrossed Lines
+
+from typing import List
+
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        # longest commen subsequence
+        m, n = len(nums1), len(nums2)
+        dp = [[0] * (m+1) for _ in range(n+1)]
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if nums2[i-1] == nums1[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[n][m]                    
